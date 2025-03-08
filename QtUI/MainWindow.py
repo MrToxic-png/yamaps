0bdaaa7ec997bcbca580f879a1cc0c1166e9ac29
buj17 <igor.ssobolev.06@gmail.com>
2025-03-08 17:47:43 +0300

    task3

--- QtUI/MainWindow.py
+++ QtUI/MainWindow.py
@@ -17,9 +17,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.image.setPixmap(QPixmap(QImage.fromData(self.point_map.get_image())))
 
     def keyPressEvent(self, a0):
-        if a0.key() == Qt.Key.Key_PageUp:
-            self.point_map.increase_zoom()
-        if a0.key() == Qt.Key.Key_PageDown:
-            self.point_map.decrease_zoom()
+        match a0.key():
+            case Qt.Key.Key_PageUp:
+                self.point_map.increase_zoom()
+            case Qt.Key.Key_PageDown:
+                self.point_map.decrease_zoom()
+            case Qt.Key.Key_Up:
+                self.point_map.move_up()
+            case Qt.Key.Key_Down:
+                self.point_map.move_down()
+            case Qt.Key.Key_Left:
+                self.point_map.move_left()
+            case Qt.Key.Key_Right:
+                self.point_map.move_right()
+
+        print(self.point_map._longitude, self.point_map._latitude)
 
         self.render_map()
--- yandex_maps_api/PointMap.py
+++ yandex_maps_api/PointMap.py
@@ -3,18 +3,40 @@ from .static_maps import get_static_maps_image
 
 START_POINT = (37.617698, 55.755864)
 START_ZOOM = 10
+BASE_OFFSET = 64
 
 
 class PointMap:
     def __init__(self):
-        self._point = START_POINT
+        self._longitude, self._latitude = START_POINT
         self._zoom = START_ZOOM
 
     def get_image(self) -> bytes:
-        return get_static_maps_image(*self._point, self._zoom)
+        return get_static_maps_image(self._longitude, self._latitude, self._zoom)
 
     def decrease_zoom(self):
         self._zoom = max(self._zoom - 1, 0)
 
     def increase_zoom(self):
         self._zoom = min((self._zoom + 1, 21))
+
+    def move_up(self):
+        self._latitude += BASE_OFFSET / 2 ** self._zoom
+        if self._latitude > 90:
+            self._latitude = 90
+
+    def move_down(self):
+        self._latitude -= BASE_OFFSET / 2 ** self._zoom
+        if self._latitude < -90:
+            self._latitude = -90
+
+    def move_right(self):
+        self._longitude += BASE_OFFSET / 2 ** self._zoom
+        if self._longitude > 180:
+            self._longitude -= 360
+
+    def move_left(self):
+        self._longitude -= BASE_OFFSET / 2 ** self._zoom
+        if self._longitude < -180:
+            self._longitude += 360
+
