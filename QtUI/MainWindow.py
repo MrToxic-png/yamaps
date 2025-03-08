5967ab9b206f256ed2d571502209697b228efd11
buj17 <igor.ssobolev.06@gmail.com>
2025-03-08 17:08:25 +0300

    task2

--- QtUI/MainWindow.py
+++ QtUI/MainWindow.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtGui import QPixmap, QImage
+from  PyQt6.QtCore import Qt
 from .ui import Ui_MainWindow
 from yandex_maps_api import PointMap
 
@@ -14,3 +15,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def render_map(self):
         self.image.setPixmap(QPixmap(QImage.fromData(self.point_map.get_image())))
+
+    def keyPressEvent(self, a0):
+        if a0.key() == Qt.Key.Key_PageUp:
+            self.point_map.increase_zoom()
+        if a0.key() == Qt.Key.Key_PageDown:
+            self.point_map.decrease_zoom()
+
+        self.render_map()
--- yandex_maps_api/PointMap.py
+++ yandex_maps_api/PointMap.py
@@ -12,3 +12,9 @@ class PointMap:
 
     def get_image(self) -> bytes:
         return get_static_maps_image(*self._point, self._zoom)
+
+    def decrease_zoom(self):
+        self._zoom = max(self._zoom - 1, 0)
+
+    def increase_zoom(self):
+        self._zoom = min((self._zoom + 1, 21))
